5e57b2bfd144cdee4e95a0c3ae313b6582cb93be
riturajsingh02 <124920823+riturajsingh02@users.noreply.github.com>
2026-09-17 20:10:40 +0530

    Delete js directory

--- js/core/state.js
+++
@@ -1,8 +0,0 @@
-/* =========================================================
-   2. APPLICATION STATE
-   ========================================================= */
-let cart = JSON.parse(localStorage.getItem('thecandleier_cart')) || [];
-let wishlist = JSON.parse(localStorage.getItem('thecandleier_wishlist')) || [];
-let activeCategory = 'all';
-let selectedPaymentMethod = 'prepaid';
-let activePdpProductId = null;
